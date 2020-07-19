459920e6de73a04e32f3f8b83095cc12788d3544
nikolina-marinova3 <56189511+nikolina-marinova3@users.noreply.github.com>
2020-07-19 20:52:04 +0100

    Visual Design
    Visual Design - initial commit

---
+++ primary/js/script.js
@@ -0,0 +1,25 @@
+// SHOW MOBILE MENU
+
+(function() {
+  const menuInfo = document.getElementById("menu-info");
+  const menuInfoClose = document.getElementById("menu-info-close");
+  const menu = document.getElementById("mobile-menu");
+    
+  // show menu
+  menuInfo.addEventListener("click", function() {
+    menu.classList.toggle("show-menu");
+  });
+  // close menu
+  menuInfoClose.addEventListener("click", function() {
+    menu.classList.toggle("show-menu"); 
+  });
+})();
+
+
+
+
+
+
+
+
+
